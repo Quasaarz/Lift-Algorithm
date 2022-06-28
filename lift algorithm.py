88460fd899a4d9effdde33fe97877d1ad86f8705
Quasaarz <63438816+Quasaarz@users.noreply.github.com>
2022-06-28 19:47:37 +0530

    Update lift algorithm.py

--- lift algorithm.py
+++ lift algorithm.py
@@ -1,7 +1,9 @@
 import time
 i = -4 
 def lift(a,b):
-       while i >a:
+       global i
+       print("Lift is at", i,"th Floor")
+       while i > a:
               print(i)
               i-=1
               time.sleep(0.5)
@@ -13,10 +15,10 @@ def lift(a,b):
                      print("Door Closing")
                      while i > b:
                             print("Going down")
-                            print(i)
                             i-=1
+                            print(i)
                             time.sleep(0.5)
-                            if i-1 ==  b:
+                            if i-1 ==  b or i == b:
                                    i=b
                                    print(i)
                                    print("Door opening")
@@ -24,11 +26,12 @@ def lift(a,b):
                                    print("Floor Reached")
                                    time.sleep(0.5)
                                    print("Door closing")
+                                   break
                                    
                      while i<b:
                             print("moving up")
-                            print(i)
                             i+=1
+                            print(i)
                             time.sleep(0.5)
                             if i+1 ==  b:
                                    i=b
@@ -38,11 +41,101 @@ def lift(a,b):
                                    print("Floor Reached")
                                    time.sleep(0.5)
                                    print("Door closing")
+                                   break
+                     if i ==b:
+                            print("Door opening")
+                            time.sleep(1)
+                            print("Floor Reached")
+                            time.sleep(0.5)
+                            print("Door closing")
+                            break
+       while i == a:
+              print(i)
+              print("Door Opening")
+              time.sleep(1)
+              print("Door Closing")
+              while i > b:
+                     print("Going down")
+                     i-=1
+                     print(i)
+                     time.sleep(0.5)
+                     if i-1 ==  b or i == b:
+                            i=b
+                            print(i)
+                            print("Door opening")
+                            time.sleep(1)
+                            print("Floor Reached")
+                            time.sleep(0.5)
+                            print("Door closing")
+                            break
+                            
+              while i<b:
+                     print("moving up")
+                     i+=1
+                     print(i)
+                     time.sleep(0.5)
+                     if i+1 ==  b:
+                            i=b
+                            print(i)
+                            print("Door opening")
+                            time.sleep(1)
+                            print("Floor Reached")
+                            time.sleep(0.5)
+                            print("Door closing")
+                            break
+              if i ==b:
+                     print("Door opening")
+                     time.sleep(1)
+                     print("Floor Reached")
+                     time.sleep(0.5)
+                     print("Door closing")
+                     break
+              elif i-1 == a:
+                     i = a
+                     print(i)
+                     print("Door Opening")
+                     time.sleep(1)
+                     print("Door Closing")
+                     while i > b:
+                            print("Going down")
+                            i-=1
+                            print(i)
+                            time.sleep(0.5)
+                            if i-1 ==  b or i == b:
+                                   i=b
+                                   print(i)
+                                   print("Door opening")
+                                   time.sleep(1)
+                                   print("Floor Reached")
+                                   time.sleep(0.5)
+                                   print("Door closing")
+                                   break
                                    
+                     while i<b:
+                            print("moving up")
+                            i+=1
+                            print(i)
+                            time.sleep(0.5)
+                            if i+1 ==  b:
+                                   i=b
+                                   print(i)
+                                   print("Door opening")
+                                   time.sleep(1)
+                                   print("Floor Reached")
+                                   time.sleep(0.5)
+                                   print("Door closing")
+                                   break
+                     if i ==b:
+                            print("Door opening")
+                            time.sleep(1)
+                            print("Floor Reached")
+                            time.sleep(0.5)
+                            print("Door closing")
+                            break
               
        while i < a:
-              print(i)
               i+=1
+              print(i)
               time.sleep(0.5)
               if i+1 == a:
                      i=a
@@ -52,8 +145,8 @@ def lift(a,b):
                      print("Door Closing")
                      while i > b:
                             print("Going down")
-                            print(i)
                             i-=1
+                            print(i)
                             time.sleep(0.5)
                             if i-1 ==  b:
                                    i=b
@@ -63,11 +156,21 @@ def lift(a,b):
                                    print("Floor Reached")
                                    time.sleep(0.5)
                                    print("Door closing")
+                                   break
+                     if i ==b:
+                            print("Door opening")
+                            time.sleep(1)
+                            print("Floor Reached")
+                            time.sleep(0.5)
+                            print("Door closing")
+                            break
+                            
+                            
                                    
                      while i<b:
                             print("moving up")
-                            print(i)
                             i+=1
+                            print(i)
                             time.sleep(0.5)
                             if i+1 ==  b:
                                    i=b
@@ -77,8 +180,9 @@ def lift(a,b):
                                    print("Floor Reached")
                                    time.sleep(0.5)
                                    print("Door closing")
-     
-       
+                                   break
+                            
+                            
               
 while True:
        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
@@ -86,7 +190,7 @@ while True:
        d = int(input("enter your destination floor(lobby is 0 and parking areas mention with a negative number counting down from the lobby(eg. If you want to go to P2 you type -3 if lobby is at P5))"))
        print(c, ",", d)
        print("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-       if c!=d:
+       if c!=d and c<=51 and c >= -4 and d<= 51 and d>=-4:
               lift(c, d)
        else:
               print("??")
